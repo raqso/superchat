47d7b0d3ff7c19b54b497ea77b0ff572fc0a8097
Adam <raqsasg@gmail.com>
2020-09-24 01:03:43 +0200

    Add Logo

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,15 @@ import { ChatRoom } from "./components/ChatRoom/ChatRoom";
 
 import { auth } from "./firebase";
 import { SignInOut } from "./components/SignInOut/SignInOut";
+import { Logo } from "./components/Logo/Logo";
 
 function App() {
 	const [user] = useAuthState(auth);
 
 	return (
 		<div className="App">
-			<header className="bg-gray-900 p-4 flex justify-end">
+			<header className="bg-gray-900 p-4 flex justify-between">
+				<Logo />
 				<SignInOut />
 			</header>
 
