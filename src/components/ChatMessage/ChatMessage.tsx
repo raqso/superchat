ee6f49cb51a0533aab8d3ece8d969b2023e382a5
Adam <raqsasg@gmail.com>
2020-09-24 01:37:28 +0200

    Hadnle displaying own messages

--- src/components/ChatMessage/ChatMessage.tsx
+++ src/components/ChatMessage/ChatMessage.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { auth } from "../../firebase";
+import { Message } from "../../models/Message";
 
 type Props = {
-	message: {
-		text: string;
-		photoURL: string | null;
-	};
+	message: Message;
 };
 
-export const ChatMessage = ({ message: { text, photoURL } }: Props) => {
+export const ChatMessage = ({ message: { text, photoURL, userId } }: Props) => {
+	const isCurrentUserMessage = userId === auth?.currentUser?.uid;
+
 	return (
-		<div className="flex justify-between my-8 px-4">
+		<div
+			className={`flex justify-between my-8 px-4 ${
+				isCurrentUserMessage ? "" : "flex-row-reverse"
+			}`}
+		>
 			{photoURL && (
 				<img
 					src={photoURL}
@@ -17,7 +22,13 @@ export const ChatMessage = ({ message: { text, photoURL } }: Props) => {
 					className="rounded-full h-8 w-8"
 				/>
 			)}
-			<p className="w-full mx-2 p-2 text-white bg-blue-600 rounded-md">
+			<p
+				className={`w-full mx-2 p-2 rounded-lg ${
+					isCurrentUserMessage
+						? "text-white bg-blue-600"
+						: "text-gray-900 bg-gray-100"
+				}`}
+			>
 				{text}
 			</p>
 		</div>
