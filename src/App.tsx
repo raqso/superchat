e58606d32e049cf4ea392f50d1ec8e944cd714e1
Adam <raqsasg@gmail.com>
2020-09-23 22:40:04 +0200

    Initial add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import firebase from 'firebase/app';
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+	apiKey: "AIzaSyBjqheQ1rSRmB73676O_IaYVA-xKF80zvk",
+	authDomain: "super-chat-796f1.firebaseapp.com",
+	databaseURL: "https://super-chat-796f1.firebaseio.com",
+	projectId: "super-chat-796f1",
+	storageBucket: "super-chat-796f1.appspot.com",
+	messagingSenderId: "724318582311",
+	appId: "1:724318582311:web:52cc72cb477089ac874002",
+	measurementId: "G-8KDB34TTWB",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
