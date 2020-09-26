212b38aee3cf602e927a06cc8096c841fc460ad9
Adam <raqsasg@gmail.com>
2020-09-26 15:20:16 +0200

    Change spaces to tabsssss

--- src/components/SignInOut/SignInOut.tsx
+++ src/components/SignInOut/SignInOut.tsx
@@ -4,31 +4,31 @@ import { auth } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const buttonClasses =
-  "bg-transparent hover:bg-blue-500 text-xs text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded";
+	"bg-transparent hover:bg-blue-500 text-xs text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded";
 
 export const SignInOut = () => {
-  const [user] = useAuthState(auth);
+	const [user] = useAuthState(auth);
 
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  };
+	const signInWithGoogle = () => {
+		const provider = new firebase.auth.GoogleAuthProvider();
+		auth.signInWithPopup(provider);
+	};
 
-  const signOut = () => {
-    auth.signOut();
-  };
+	const signOut = () => {
+		auth.signOut();
+	};
 
-  if (!user) {
-    return (
-      <button className={buttonClasses} onClick={signInWithGoogle}>
-        Sign in with Google
-      </button>
-    );
-  }
+	if (!user) {
+		return (
+			<button className={buttonClasses} onClick={signInWithGoogle}>
+				Sign in with Google
+			</button>
+		);
+	}
 
-  return (
-    <button className={buttonClasses} onClick={signOut}>
-      Sign out
-    </button>
-  );
+	return (
+		<button className={buttonClasses} onClick={signOut}>
+			Sign out
+		</button>
+	);
 };
