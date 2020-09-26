35d73752caf502bcdcc2d27af8dd7023a253cc71
zdzislaw <belazdzislaw@gmail.com>
2020-09-26 21:59:13 +0200

    Add classes parameters to handle header on mobile

--- src/components/SignInOut/SignInOut.tsx
+++ src/components/SignInOut/SignInOut.tsx
@@ -4,7 +4,7 @@ import { auth } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const buttonClasses =
-	"bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded";
+	"bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white ml-5 px-2 sm:py-0 sm:px-2 border border-blue-500 hover:border-transparent rounded";
 
 export const SignInOut = () => {
 	const [user] = useAuthState(auth);
