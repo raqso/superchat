4e766d5f9b76777d8e492dd7f7f99ba8682a8f4c
Adam <raqsasg@gmail.com>
2020-09-27 05:07:29 +0200

    Add removing image

--- src/components/ChatRoom/ChatRoom.tsx
+++ src/components/ChatRoom/ChatRoom.tsx
@@ -46,6 +46,7 @@ export const ChatRoom = () => {
 		await addMessage(messageDoc);
 
 		setFormValue('');
+		setFile(undefined);
 		dummyRef?.current?.scrollIntoView({ behavior: 'smooth' });
 	};
 	const handleFormValueChange = (
--- src/components/UploadPhotoButton/UploadPhotoButton.tsx
+++ src/components/UploadPhotoButton/UploadPhotoButton.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCamera } from '@fortawesome/free-solid-svg-icons';
+import { faCamera, faWindowClose } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './UploadPhotoButton.module.css';
 
@@ -22,28 +22,48 @@ export const UploadPhotoButton = ({ file, onFileChange }: Props) => {
 			const newFile = event.target.files[0];
 			setMessageImageFile(newFile);
 			onFileChange(newFile);
-
-			const previewUrl = window.URL.createObjectURL(event.target.files[0]);
-			setMessageImageUrl(previewUrl);
 		},
 		[onFileChange]
 	);
 
+	const removeImage = useCallback(() => {
+		setMessageImageFile(undefined);
+	}, []);
+
+	useEffect(() => {
+		if (!messageImageFile) {
+			return setMessageImageUrl('');
+		}
+
+		const previewUrl = window.URL.createObjectURL(messageImageFile);
+		setMessageImageUrl(previewUrl);
+	}, [messageImageFile]);
+
 	return (
-		<div className={`${styles.uploadWrapper} flex hover:opacity-75`}>
+		<div className={`${styles.uploadWrapper} flex hover:opacity-75 pa-2`}>
 			{!messageImageFile && (
 				<button className="mr-4">
 					<FontAwesomeIcon icon={faCamera} size="2x" color="#2d3748" />
 				</button>
 			)}
 			{messageImageUrl && (
-				<img
-					src={messageImageUrl}
-					alt="element to upload"
-					className={`${styles.image} mr-2`}
-				/>
+				<>
+					<button
+						onClick={removeImage}
+						className={`${styles.closeButton} hover:opacity-75`}
+					>
+						<FontAwesomeIcon icon={faWindowClose} size="1x" color="red" />
+					</button>
+					<div>
+						<img
+							src={messageImageUrl}
+							alt="element to upload"
+							className={`${styles.image} mr-2`}
+						/>
+					</div>
+				</>
 			)}
-			<input type="file" onChange={handleChange} />
+			<input type="file" onChange={handleChange} disabled={!!messageImageUrl} />
 		</div>
 	);
 };
