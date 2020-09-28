f3d74211997fff59c13602d73372772fd5a55a61
Adam Rak <raqsasg@gmail.com>
2020-09-29 00:32:22 +0200

    Turned on purge css for tailwindcss styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+	future: {
+		// removeDeprecatedGapUtilities: true,
+		// purgeLayersByDefault: true,
+	},
+	purge: {
+		content: [
+			'./src/**/*.html',
+			'./src/**/*.ts',
+			'./src/**/*.tsx',
+			'./src/**/*.js',
+			'./src/**/*.jsx',
+		],
+	},
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
