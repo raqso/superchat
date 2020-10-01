1d8614ccd8eb8ccd3495e2eb409f89cec6a2863d
zdzislaw <belazdzislaw@gmail.com>
2020-10-01 23:03:01 +0200

    Set prettier eol auto

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,7 +50,13 @@ module.exports = {
 		"@typescript-eslint/no-var-requires": "off",
 		"@typescript-eslint/explicit-module-boundary-types": "off",
 		"@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars-experimental": "error",
-    "no-unused-vars": "off"
-	},
+		"@typescript-eslint/no-unused-vars-experimental": "error",
+		"no-unused-vars": "off",
+		"prettier/prettier": [
+			"error",
+			{
+			endOfLine: "auto",
+			},
+		],
+		},
 };
