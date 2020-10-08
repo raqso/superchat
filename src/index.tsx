75a6c02c0d16326271ddb6eae4f27f71ab71a0f1
Adam <raqsasg@gmail.com>
2020-10-08 22:50:57 +0200

    Add logrocket package

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/main.css';
 import 'balloon-css';
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -12,6 +14,9 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('root')
 );
+
+LogRocket.init('xxxx11/xxxxxx');
+setupLogRocketReact(LogRocket);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
