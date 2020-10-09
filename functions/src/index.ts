9ba49c3ae300519b780606395013412931efdddb
Adam <raqsasg@gmail.com>
2020-10-09 22:13:03 +0200

    Trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
 import { onCreate } from './db/messages/onCreate';
 import { unbanUsers } from './cron/unBanUsers';
-
+// Trigger
 export { onCreate as dbMessagesOnCreate, unbanUsers as cronUnBanUsers };
